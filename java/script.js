17c514a68879482442bf90d1dcc72c2b792c11b4
Amanda Kuiack <akuiack@my.bcit.ca>
2021-04-21 13:23:09 -0700

    start event listener

---
+++ java/script.js
@@ -0,0 +1,14 @@
+const doucument.body.querySelector("div")
+
+function albumSelect(e){
+    //gets target's identity
+    let clickedImg = e.target.innerHTML
+    console.log(clickedNote)
+
+
+        }
+    }
+}
+
+
+div.addEventListener('click', albumSelect)
