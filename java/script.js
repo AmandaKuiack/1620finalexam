2c50ba6c7ee3d4be6a1cc76bc05b698a19c264f3
Amanda Kuiack <akuiack@my.bcit.ca>
2021-04-21 13:42:24 -0700

    event identify selected img

--- java/script.js
+++ java/script.js
@@ -1,12 +1,21 @@
-const doucument.body.querySelector("div")
+const div = document.querySelector(".selection");
+
+
+let albums = [{ name: "Album1", price: 12.99, addedToCart: false },
+{ name: "Album2", price: 14.99, addedToCart: false}, { name: "Album3", price: 9.99, addedToCart: false }, { name: "Album4", price: 19.99, addedToCart: false }];
+
 
 function albumSelect(e){
     //gets target's identity
-    let clickedImg = e.target.innerHTML
-    console.log(clickedNote)
+    let clickedImg = e.target.id
+    console.log(clickedImg)
+    for (let item of albums){
+        if (item.name === clickedImg){
+            console.log(`Loading contents of ${item.name}`)
+            
 
-
-
+        }
+    }
 }
 
 
